9f2b0024fd410bd3daa575c8af49c227b04de95f
JosephClay <j053phclay@gmail.com>
2021-06-15 14:20:23 -0500

    firefox cssStyleDeclaration fallback

--- src/cssStyleDeclaration.js
+++ src/cssStyleDeclaration.js
@@ -1,1 +1,7 @@
-export default Object.keys(document.createElement('div').style);
+// all browsers except firefox
+const keys = Object.keys(document.createElement('div').style);
+
+export default keys.length 
+  ? keys 
+  // firefox fallback
+  : Object.values(globalThis.getComputedStyle(globalThis.document.body));
